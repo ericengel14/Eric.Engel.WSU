a48d855b746085f165ff49a17d9c80df2b96a942
ericengel14 <43424764+ericengel14@users.noreply.github.com>
2018-11-05 16:34:23 -0800

    Added comments
    Added more detail to comments

--- Btree/Btree.h
+++ Btree/Btree.h
@@ -4,6 +4,8 @@
         Btree
 
         Header File containing class implementation of templated Btree
+        Btree will take 2 arguments and int to the max number of keys that can be stored in each node
+        and a function pointer to a compare function.
 */
 
 #pragma once
@@ -69,6 +71,7 @@ public:
         
         int (*compareFunction)(T*,T*);
 
+        void checkNewRoot(Node<T> *node, Node<T> *parent);
         void mergeOlder(Node<T> *node, Node<T> *parent);
         void mergeYounger(Node<T> *node, Node<T> *parent);
         void marriage(Node<T> *node);
@@ -92,6 +95,10 @@ public:
 };
 
 //Constructor for Btree class
+//Takes 2 arguments
+//int of max number of keys that can fit into one node
+//function pointer for the comparison function
+//must return -1 if less then, 1 if greater then, and 0 if equal too
 template <typename T>
 Btree<T>::Btree(int mKeys,T (*myFunc)(T*,T*))
 {
@@ -102,6 +109,9 @@ Btree<T>::Btree(int mKeys,T (*myFunc)(T*,T*))
 }
 
 //Costructor for Node Class
+//Takes 2 arguments
+//int of max keys that can fit in each node
+//function pointer for compare function
 template <typename T>
 Node<T>::Node(int mKey,T (*myFunc)(T*,T*))
 {
@@ -115,6 +125,7 @@ Node<T>::Node(int mKey,T (*myFunc)(T*,T*))
 }
 
 //Constructor for Key Class
+//sets rChild pointer to NULL
 template <typename T>
 Node<T>::Key::Key()
 {
@@ -122,6 +133,7 @@ Node<T>::Key::Key()
 }
 
 //function to print out tree
+//Ignore this does not work at all
 template <typename T>
 void Btree<T>::printTree()
 {       
@@ -157,6 +169,8 @@ void Btree<T>::printTree()
 }
 
 //Function to look for element inside Node
+//Takes one argument a pointer to the element to be searched for
+//returns an int a 0 if the element is a match
 template <typename T>
 int Node<T>::isMatch(T *search)
 {
@@ -177,6 +191,9 @@ int Node<T>::isMatch(T *search)
 }
 
 //Function to look for element inside Node and return an iterator to the found item
+//Takes one argument a pointer to the element to be searched for
+//Returns a key pointer to the element being searched, for it is contained in the node
+//if it is not in the node it returns a pointer to the key that is larger then the searched for item
 template <typename T>
 typename Node<T>::Key* Node<T>::findMatch(T *search)
 {
@@ -193,6 +210,8 @@ typename Node<T>::Key* Node<T>::findMatch(T *search)
 }
 
 //Function to search for an element in the Btree
+//Takes one argument a pointer to the element to be searched for
+//calls helper functions isMatch and traverseLevel
 template <typename T>
 void Btree<T>::search(T *search)
 {
@@ -242,6 +261,7 @@ int Node<T>::checkFull()
 }
 
 //Function to call checkFull function and if it is full call function to break node
+//Takes one argument a node pointer to the node that was just inserted into
 template <typename T>
 void Btree<T>::checkBreak(Node<T> *temp)
 {
@@ -256,6 +276,8 @@ void Btree<T>::checkBreak(Node<T> *temp)
 }
 
 //Function to help traversal find which pointer to take to next node
+//Takes one argument a pointer to the element to be inserted into the tree
+//Returns a node pointer to the node at the lower level in the tree
 template <typename T>
 Node<T>* Node<T>::traverseLevel(T *add)
 {
@@ -283,6 +305,8 @@ Node<T>* Node<T>::traverseLevel(T *add)
 
 
 //Function to traverse through tree until the correct place to insert new element
+//Takes one argument a pointer to the element to be inderted
+//Returns a node pointer a leaf node at the proper location for the element to be inserted
 template <typename T>
 Node<T>* Btree<T>::iTraversal(T *add)
 {
@@ -297,6 +321,7 @@ Node<T>* Btree<T>::iTraversal(T *add)
 }
 
 //Function to find position to add new element into Node
+//Takes one argument a pointer to the element to be added 
 //Return iterator pointer to position to be inserted into
 template <typename T>
 typename list<typename Node<T>::Key>::iterator Node<T>::iPosition(T *add)
@@ -314,6 +339,7 @@ typename list<typename Node<T>::Key>::iterator Node<T>::iPosition(T *add)
 }
 
 //Function to break root node
+//Takes one argument a node pointer to the node to be broken
 template <typename T>
 void Btree<T>::breakRoot(Node<T> *node)
 {
@@ -347,6 +373,7 @@ void Btree<T>::breakRoot(Node<T> *node)
 }
 
 //Function to preform addKey but taking a key as perameter not just the element to be added
+//Takes one argument a key pointer to the element to be inserted into the list
 template <typename T>
 void Node<T>::pushKey(Key *key)
 {
@@ -357,6 +384,8 @@ void Node<T>::pushKey(Key *key)
 }
 
 //Function to break a node with too many elements
+//Takes one argument a node pointer to the node that is too full and needs to be broken
+//calls helper fuctions breakRoot, pushKey, and checkBreak
 template <typename T>
 void Btree<T>::breakNode(Node<T> *node)
 {
@@ -394,6 +423,8 @@ void Btree<T>::breakNode(Node<T> *node)
 }
 
 //Function to addKey to node
+//Takes one argument a pointer to the element to be inserted into the tree
+//calls helper function iPosition
 template <typename T>
 void Node<T>::addKey(T *add)
 {
@@ -416,6 +447,8 @@ void Node<T>::addKey(T *add)
 }
 
 //Function to insert item into Btree
+//Takes one argument of a pointer to the element to be added
+//calls helper functions addKey, checkBreak, and iTraversal
 template <typename T>
 void Btree<T>::insert(T *add)
 {
@@ -448,6 +481,7 @@ void Btree<T>::insert(T *add)
 }
 
 //Function to delete leaf level Key
+//Takes one argument a pointer to the element to be deleted
 template <typename T>
 void Node<T>::deleteLeaf(T *deletion)
 {
@@ -467,6 +501,8 @@ void Node<T>::deleteLeaf(T *deletion)
 }
 
 //function to check if current Node has an extra key
+//Returns a 0 if there are no extra keys
+//Returns a 1 if there is an extra key
 template <typename T>
 int Node<T>::extra()
 {
@@ -485,6 +521,7 @@ int Node<T>::extra()
 }
 
 //Function to check if left sibling has an extra key to steal
+//Takes one argument a pointer to an element contained within the main node
 //returns a 0 if theft is possible from youngerBrother
 template <typename T>
 bool Node<T>::youngerBrother(T *element)
@@ -505,6 +542,7 @@ bool Node<T>::youngerBrother(T *element)
 }
 
 //Function to check if right sibling has an extra key
+//Takes one argument a pointer to an element contained within the main node
 //Returns a 1 if theft is possible from olderBrother
 template <typename T>
 bool Node<T>::olderBrother(T *element)
@@ -522,6 +560,7 @@ bool Node<T>::olderBrother(T *element)
 }
 
 //Function to check if you can steal a key from neighbor node
+//Takes one argument a node pointer to the node with too few keys
 //Returns -1 if left sibling can be stolen from
 //Returns 1 if right sibling can be stolen from
 //Returns 0 if neither right or left sibling can be stolen from
@@ -582,7 +621,10 @@ int Btree<T>::caseJoint(Node<T> *node)
 }
 
 //Functino to steal key from neighbor node
-//Takes a bool argument 0 if younger brother 1 if older brother
+//Takes 3 arguments
+//a bool argument 0 if younger brother 1 if older brother
+//a node pointer to node with too few keys
+//a node to parent of node
 template <typename T>
 void Btree<T>::theft(bool brother, Node<T> *node, Node<T> *parent)
 {
@@ -607,7 +649,32 @@ void Btree<T>::theft(bool brother, Node<T> *node, Node<T> *parent)
         } 
 }
 
-//Function to marge with right sibling
+//Function to check for case if all keys are merged into just one node
+//Takes 2 arguments
+//node pointer to node elements were merged into
+//node pointer to parent of node
+template <typename T>
+void Btree<T>::checkNewRoot(Node<T> *node, Node<T> *parent)
+{
+        int count = 0;
+
+        if(root == parent)
+        {
+                for(parent->it1 = parent->first.begin(); parent->it1 != parent->first.end(); ++parent->it1, count++){}     //Counting how many elements are in the list
+
+                if(count == 0)                         //If only one element in the root
+                {
+                        root = node;
+                        node->prev = NULL;
+                }
+        }
+}
+
+//Function to merge with right sibling
+//Takes 2 arguments 
+//node pointer to the node to merge into
+//node pointer of parent node
+//Calls helper function checkNewRoot after merge is complete
 template <typename T>
 void Btree<T>::mergeOlder(Node<T> *node, Node<T> *parent)
 {
@@ -635,9 +702,15 @@ void Btree<T>::mergeOlder(Node<T> *node, Node<T> *parent)
 
         it2 = sibling->first.end();
         sibling->first.erase(sibling->it1, it2);         //erase all the keys in sibling
+
+        checkNewRoot(node, parent);
 }
 
 //Function to merge with left sibling if there is no left sibling
+//Takes 2 arguments
+//Node pointer to node containing not enough keys
+//node pointer to parent of node
+//moves to left sibling of node and then calls mergeOlder on that node
 template <typename T>
 void Btree<T>::mergeYounger(Node<T> *node, Node<T> *parent)
 {
@@ -653,6 +726,8 @@ void Btree<T>::mergeYounger(Node<T> *node, Node<T> *parent)
 }
 
 //Function to merge siblings into one family if no siblings have keys to share
+//Takes argument of node pointer to the node elements are to be merged into
+//calls helper function mergeYounger, mergeOlder
 template <typename T>
 void Btree<T>::marriage(Node<T> *node)
 {
@@ -672,7 +747,9 @@ void Btree<T>::marriage(Node<T> *node)
         }
 }
 
-//Function to perform check if Node is empty then call correct functino to fix problem
+//Function to perform check if Node is empty then call correct function to fix problem
+//Takes argument pointing to node, wanting to check if it is empty
+//Calls helper function checkEmpty, caseJoint, and marriage
 template <typename T>
 void Btree<T>::checkMin(Node<T> *node)
 {
@@ -693,6 +770,8 @@ void Btree<T>::checkMin(Node<T> *node)
 }
 
 //Function to check if leaf node has too few elements after deletion
+//Does not take any arguments
+//Returns a 0 if there are enough elements in the node and a 1 if there are too few elements in the node
 template <typename T>
 int Node<T>::checkEmpty()
 {
@@ -713,6 +792,10 @@ int Node<T>::checkEmpty()
 }
 
 //Function to find immediate successor
+//Takes 2 arguments
+//a node pointer to the node containing the key to be deleted
+//the element to be deleted
+//Calls helper function findMatch
 template <typename T>
 typename Node<T>::Key* Btree<T>::successor(Node<T> *node,T *deletion)
 {
@@ -728,12 +811,17 @@ typename Node<T>::Key* Btree<T>::successor(Node<T> *node,T *deletion)
 }
 
 //Function to swap element to be deleted and its immediate successor and then call function to delete element
+//Takes 3 arguments
+//keyD a pointer to the item to be deleted
+//KeyS a pointer to the immediate successor to keyD
+//and a pointer to the element to be deleted from the tree
+//Calls helper functions deleteLeaf and checkMin
 template <typename T>
 void Btree<T>::swap(typename Node<T>::Key *keyD, typename Node<T>::Key *keyS, T *deletion)
 {
         T tempD;
-        tempD = keyS->data;
-        keyS->data = keyD->data;
+        tempD = keyS->data;                     //Swapping data 
+        keyS->data = keyD->data;                
         keyD->data = tempD;
         
         temporary->deleteLeaf(deletion);
@@ -741,6 +829,8 @@ void Btree<T>::swap(typename Node<T>::Key *keyD, typename Node<T>::Key *keyS, T 
 }
 
 //Function to set immediate to point at node containing immediate successor
+//Takes a Key pointer to the parent element of the leaf node I swap keys with to perform delete
+//of an element not at the leaf level
 template <typename T>
 void Btree<T>::setTemp(typename Node<T>::Key *key)
 {
@@ -754,6 +844,8 @@ void Btree<T>::setTemp(typename Node<T>::Key *key)
 }
 
 //Function to delete item from Btree
+//Takes a pointer to the element to be deleted from the tree
+//Calls Helper functions search, deleteLeaf, checkMin, successor,findMatch, setTemp, and swap
 template <typename T>
 void Btree<T>::deleteB(T *deletion)
 {
